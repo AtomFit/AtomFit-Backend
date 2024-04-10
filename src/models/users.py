41bf38aeec319e837a44ab224df6e6c1ee538991
stanislavCasciuc <stanislav.casciuc@gmail.com>
2024-04-10 16:17:20 +0300

    Feat: Add user sqlalchemy model

--- src/models/users.py
+++ src/models/users.py
@@ -1,8 +1,31 @@
+from datetime import datetime
 from pydantic import EmailStr
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.database import Base
 
-class User(Base):
+
+
+class UserOrm(Base):
+    __tablename__ = "user"
     username: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[EmailStr] = mapped_column(nullable=False, unique=True)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(nullable=False)
+    is_male: Mapped[bool] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(server_default=("TIMEZONE('utc', now())"))
+
+    user_metrics: Mapped["UserMetricsOrm"] = relationship(back_populates="user")
+
+class UserMetricsOrm(Base):
+    __tablename__ = "user_metrics"
+    height: Mapped[float] = mapped_column(nullable=False)
+    weight: Mapped[float] = mapped_column(nullable=False)
+    age: Mapped[int] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    goal: Mapped[str] = mapped_column(nullable=False)
+    weight_preference: Mapped[float] = mapped_column(nullable=False)
+
+    user: Mapped["UserOrm"] = relationship(back_populates='user_metrics')
