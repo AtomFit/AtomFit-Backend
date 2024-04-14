86ad4a7cb524fecf07d6a8e17448f31f6ab04521
stanislavCasciuc <stanislav.casciuc@gmail.com>
2024-04-14 19:54:13 +0300

    Feat(auth): Login and current_user, refactor auth service

--- src/api/routers/auth.py
+++ src/api/routers/auth.py
@@ -1,29 +1,46 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, Response
+from fastapi import APIRouter, Depends, Response, Request
 
+from api.dependencies import get_auth_service
 from schemas.users import RegisterUserSchema, LoginUserSchema
 from services.auth import AuthService
-from utils.unit_of_work import IUnitOfWork, UnitOfWork
+
 
 router = APIRouter()
 
 
 @router.post("/register", tags=["auth"])
-async def register(user_data: RegisterUserSchema, uow: Annotated[IUnitOfWork, Depends(UnitOfWork)],
-                   auth_service: Annotated[AuthService, Depends(AuthService)]):
-    user_id = await auth_service.register(user_data=user_data, uow=uow)
+async def register(
+    user_data: RegisterUserSchema,
+    auth_service: Annotated[AuthService, Depends(get_auth_service)],
+):
+    user_id = await auth_service.register(user_data=user_data)
     return {"user_id": user_id}
 
 
 @router.post("/login", tags=["auth"], response_model=dict)
-async def login(user_data: LoginUserSchema, response: Response, uow: Annotated[IUnitOfWork, Depends(UnitOfWork)],
-                auth_service: Annotated[AuthService, Depends(AuthService)]):
-    token_pair = await auth_service.login(data=user_data, uow=uow, response=response)
+async def login(
+    user_data: LoginUserSchema,
+    response: Response,
+    auth_service: Annotated[AuthService, Depends(get_auth_service)],
+):
+    token_pair = await auth_service.login(data=user_data, response=response)
     return token_pair
 
 
 @router.post("/logout", tags=["auth"])
-async def logout(response: Response):
-    response.delete_cookie(key=AuthService.ACCESS_TOKEN)
-    response.delete_cookie(key=AuthService.REFRESH_TOKEN)
+async def logout(
+    response: Response, auth_service: Annotated[AuthService, Depends(get_auth_service)]
+):
+    auth_service.logout(response=response)
+    return {"result": "success"}
+
+
+@router.post("/refresh", tags=["auth"])
+async def refresh(
+    request: Request,
+    response: Response,
+    auth_service: Annotated[AuthService, Depends(get_auth_service)],
+):
+    await auth_service.refresh(response=response, request=request)
     return {"result": "success"}
--- src/services/auth.py
+++ src/services/auth.py
@@ -1,46 +1,43 @@
-from fastapi import Response
-
+from typing import Final
+import bcrypt
+from fastapi import Response, Request
 
 from exeptions.users import EmailAlreadyExistsException, UserNotFoundException
-from schemas.tokens import TokenPairSchema
 from schemas.users import (
-    UserMetricsSchema,
     UserSchema,
     RegisterUserSchema,
     LoginUserSchema,
 )
-from services.tokens import TokenService
+from services.tokens import TokenEncoderService, TokenDecoderService
 from services.users import UserService
 from utils.unit_of_work import IUnitOfWork
 
 
 class AuthService:
-    ACCESS_TOKEN = "access"
-    REFRESH_TOKEN = "refresh"
+    ACCESS_TOKEN: Final[str] = "access"
+    REFRESH_TOKEN: Final[str] = "refresh"
 
-    async def register(self, user_data: RegisterUserSchema, uow: IUnitOfWork) -> int:
-        user_data_dict = self.get_user_data_dict(user_data.user)
-        user_metrics_data_dict = self.get_user_metrics_data_dict(user_data.user_metrics)
+    def __init__(self, uow: IUnitOfWork):
+        self.uow = uow
+        self.user_service = UserService(uow=self.uow)
 
-        user = await self.get_user(user_data_dict["email"], uow)
+    async def register(self, user_data: RegisterUserSchema) -> int:
+        user_data_dict = self.get_user_data_dict(user_data)
+        user = await self.get_user(user_data_dict["email"])
         if user:
             raise EmailAlreadyExistsException
 
-        async with uow:
-            user_id = await uow.users.add_one(data=user_data_dict)
-            user_metrics_data_dict["user_id"] = user_id
-            await uow.user_metrics.add_one(data=user_metrics_data_dict)
-            await uow.commit()
+        async with self.uow:
+            user_id = await self.uow.users.add_one(data=user_data_dict)
+            await self.uow.commit()
         return user_id
 
-    async def login(
-        self, data: LoginUserSchema, uow: IUnitOfWork, response: Response
-    ) -> dict:
-        user = await self.get_user(data.email, uow)
+    async def login(self, data: LoginUserSchema, response: Response) -> dict:
+        user = await self.get_user(data.email)
         if user is None:
             raise UserNotFoundException
 
-        token_service = TokenService(user)
+        token_service = TokenEncoderService(user)
         access_token = token_service.create_access_token()
         refresh_token = token_service.create_refresh_token()
         response.set_cookie(
@@ -59,30 +56,49 @@ class AuthService:
         )
         return {"result": "success"}
 
-    async def get_user(self, email: str, uow: IUnitOfWork):
-        user_service = UserService()
-        async with uow:
-            user = await user_service.get_user(data={"email": email}, uow=uow)
+    def logout(self, response: Response):
+        response.delete_cookie(key=self.ACCESS_TOKEN)
+        response.delete_cookie(key=self.REFRESH_TOKEN)
+
+    async def refresh(self, response: Response, request: Request):
+        refresh_token = request.cookies.get(self.REFRESH_TOKEN)
+        token_service = TokenDecoderService(refresh_token, uow=self.uow)
+        user = await token_service.get_current_active_user()
+
+        token_service = TokenEncoderService(user)
+        access_token = token_service.create_access_token()
+        response.set_cookie(
+            key=self.ACCESS_TOKEN,
+            value=access_token,
+            httponly=True,
+            secure=True,
+            samesite="strict",
+        )
+
+    async def get_user(self, email: str):
+        user_service = UserService(uow=self.uow)
+        async with self.uow:
+            user = await user_service.get_user(data={"email": email})
         if user:
             return user
 
     def get_user_data_dict(self, user_data: UserSchema) -> dict:
-        hashed_password = get_hashed_password(user_data.password)
+        hashed_password = self.hash_password(user_data.password)
         return {
             "email": user_data.email,
             "hashed_password": hashed_password,
             "username": user_data.username,
+            "age": user_data.age,
+            "height": user_data.height,
+            "weight": user_data.weight,
+            "goal": user_data.goal.value,
+            "weight_preference": user_data.weight_preference,
+            "is_male": user_data.is_male,
         }
 
-    def get_user_metrics_data_dict(self, user_metrics_data: UserMetricsSchema) -> dict:
-        return {
-            "age": user_metrics_data.age,
-            "height": user_metrics_data.height,
-            "weight": user_metrics_data.weight,
-            "goal": user_metrics_data.goal.value,
-            "weight_preference": user_metrics_data.weight_preference,
-            "is_male": user_metrics_data.is_male,
-        }
-
-    def get_hashed_password(password: str) -> str:
-        return hash_password(password).decode("utf-8")
+    @staticmethod
+    def hash_password(password: str) -> str:
+        salt = bcrypt.gensalt()
+        pwd_bytes: bytes = password.encode()
+        hashed_password = bcrypt.hashpw(pwd_bytes, salt)
+        return hashed_password.decode("utf-8")
