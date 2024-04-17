a43c85615b15cac184a9320f39ded29efe296f51
stanislavCasciuc <stanislav.casciuc@gmail.com>
2024-04-17 13:34:21 +0300

    Feat(depends): add user service depends

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -3,6 +3,7 @@ from fastapi import Cookie
 from schemas.users import UserSchema
 from services.tokens import TokenDecoderService
 from services.auth import AuthService
+from services.users import UserService
 from utils.unit_of_work import UnitOfWork, IUnitOfWork
 
 
@@ -16,3 +17,9 @@ async def get_current_active_user(access: str = Cookie(None)) -> UserSchema:
     uow: IUnitOfWork = UnitOfWork()
     token_service: TokenDecoderService = TokenDecoderService(access, uow=uow)
     return await token_service.get_current_active_user()
+
+
+def get_user_service() -> UserService:
+    uow: IUnitOfWork = UnitOfWork()
+    user_service: UserService = UserService(uow)
+    return user_service
