3cc3ae49a1290f1adf19046d17b3a9693b7e7d5d
stanislavCasciuc <stanislav.casciuc@gmail.com>
2024-04-21 22:06:39 +0300

    Feat(main): Add cors middleware

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.routers import routers
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,  # type: ignore
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 for router in routers:
     app.include_router(router)
@@ -13,4 +24,5 @@ async def root() -> dict[str, str]:
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
