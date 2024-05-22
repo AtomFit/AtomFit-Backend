026973e156035bb1bf39a7a5ec58b61ad987b9b9
stanislavCasciuc <stanislav.casciuc@gmail.com>
2024-05-22 15:55:01 +0300

    Update(dependencies)

--- src/services/tokens.py
+++ src/services/tokens.py
@@ -4,7 +4,6 @@ import jwt
 from fastapi import HTTPException, status
 from jwt import InvalidTokenError
 
-
 from config import settings
 from exeptions.users import InactiveUserException
 from schemas.users import UserSchema
